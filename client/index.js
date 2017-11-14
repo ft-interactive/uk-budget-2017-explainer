78016c60fa4f2aee4a941f8253403f1aeb617946
Callum Locke <callum.locke@gmail.com>
2017-11-14 15:54:53 +0000

    manual stickiness

--- client/index.js
+++ client/index.js
@@ -1,4 +1,53 @@
 import './styles.scss';
-import Sticky from 'sticky-js';
 
-const sticky = new Sticky('.deficit-chart__figure');
+const chartContainer = document.querySelector('.deficit-chart');
+const chart = chartContainer.querySelector('.deficit-chart__figure');
+
+const unstickMarker = document.querySelector('[data-marker=unstick]');
+
+const setStuck = () => {
+  chart.classList.add('deficit-chart__figure--stuck');
+  chart.classList.remove('deficit-chart__figure--at-bottom');
+  chart.style.top = null;
+};
+
+const setAtTopUnstuck = () => {
+  chart.classList.remove('deficit-chart__figure--stuck');
+  chart.classList.remove('deficit-chart__figure--at-bottom');
+  chart.style.top = null;
+};
+
+const setAtBottomUnstuck = (offset) => {
+  chart.classList.add('deficit-chart__figure--at-bottom');
+  chart.classList.remove('deficit-chart__figure--stuck');
+  chart.style.top = `${offset}px`;
+};
+
+const updateDisplay = () => {
+  // stick or unstick the chart as appropriate
+  {
+    const chartContainerBB = chartContainer.getBoundingClientRect();
+
+    if (chartContainerBB.top < 0) {
+      // we're under the point where the chart first sticks.
+      // check we're not somewhere after the unstick marker...
+      const unstickMarkerBB = unstickMarker.getBoundingClientRect();
+
+      if (chartContainerBB.height - unstickMarkerBB.top > 0) {
+        setAtBottomUnstuck(unstickMarkerBB.top - chartContainerBB.height);
+      } else {
+        setStuck();
+      }
+    } else {
+      // we're somewhere near the top of the page; chart shouldn't be stuck here
+      setAtTopUnstuck();
+    }
+  }
+};
+
+// update the display once at the start
+updateDisplay();
+
+// ...and again on every scroll and resize
+window.addEventListener('scroll', updateDisplay);
+window.addEventListener('scroll', updateDisplay);
