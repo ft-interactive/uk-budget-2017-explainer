e047138bdacfc8af3a24fc5f0135aae5f5940e98
Callum Locke <callum.locke@gmail.com>
2017-11-18 13:08:05 +0000

    add fiscal-cap-marker; improve headroom label

--- client/components/Chart/Bars.js
+++ client/components/Chart/Bars.js
@@ -60,20 +60,35 @@ const Bars = ({
             {labels[i]}
           </div>
 
+          {/* a 'well' under the bar indicating the available length until the cap */}
           {isCapYear ? (
-            <div
-              className="headroom"
-              style={{
-                [vertical ? 'height' : 'width']: `${headroomLength}%`,
-                [vertical ? 'bottom' : 'left']: `${valueLength}%`,
-              }}
-            >
-              <div className="headroom-label">{`£${Math.round((fiscalCap - value) * 10) /
-                10}bn`}</div>
+            <div>
+              <div
+                className="bar-well"
+                style={{ [vertical ? 'height' : 'width']: `${fiscalCapLength}%` }}
+              />
+              <div
+                className="headroom-label"
+                style={{
+                  [vertical ? 'bottom' : 'left']: `${valueLength}%`,
+                  [vertical ? 'height' : 'width']: `${headroomLength}%`,
+                }}
+              >{`£${Math.round((fiscalCap - value) * 10) / 10}bn`}</div>
             </div>
           ) : null}
 
+          {/* the actual, filled bar */}
           <div className="bar" style={{ [vertical ? 'height' : 'width']: `${valueLength}%` }} />
+
+          {/* cap marker */}
+          {isCapYear ? (
+            <div
+              className="fiscal-cap-marker"
+              style={{ [vertical ? 'bottom' : 'left']: `${fiscalCapLength}%` }}
+            >
+              Fiscal cap
+            </div>
+          ) : null}
         </div>
       );
     })}
@@ -97,7 +112,7 @@ const Bars = ({
         text-transform: uppercase;
       }
 
-      .headroom {
+      .bar-well {
         position: absolute;
         height: 100%;
         top: 0;
@@ -106,28 +121,47 @@ const Bars = ({
         box-shadow: inset 0px 1px 4px 0px rgba(0, 0, 0, 0.2);
         background: #e1e0df;
         opacity: 0;
-        transition: opacity 0.15s ease-in 0s;
+        transition: opacity 0.15s ease-in;
       }
 
-      .bars--show-cap .headroom {
+      .bars--show-cap .bar-well {
         opacity: 1;
+        transition: opacity 0.25s ease-in;
       }
 
       .headroom-label {
-        transition: opacity 0.2s ease-in 0s;
+        transition: opacity 0.05s ease-in;
         opacity: 0;
-        position: absolute;
-      }
-
-      .bars--highlight-cap .headroom-label {
-        opacity: 1;
         width: 100%;
         text-align: center;
         font-size: 28px;
         font-weight: 600;
-        position: relative;
+        position: absolute;
         top: -9px;
         color: #444;
+      }
+
+      .bars--highlight-cap .headroom-label {
+        opacity: 1;
+        transition: opacity 0.45s ease-in 0.3s;
+      }
+
+      .fiscal-cap-marker {
+        opacity: 0;
+        transition: opacity 0.05s ease-in;
+        border-left: 4px solid black;
+        padding: 2px 0 0 4px;
+        height: 30px;
+        position: absolute;
+        top: -${(30 - 15) / 2}px;
+        text-transform: uppercase;
+        width: 20px;
+        font-size: 13px;
+        line-height: 13px;
+      }
+
+      .bars--show-cap .fiscal-cap-marker {
+        opacity: 1;
       }
 
       .bar {
